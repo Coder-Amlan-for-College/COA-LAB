f5a6d45edb0c12768da39c55d67ba78cb53e5126
Coder-Amlan-for-College <nayakamlan188@gmail.com>
2025-03-24 09:59:40 +0530

    Add files via upload

---
+++ LAB 3/OBJECTIVE1.asm
@@ -0,0 +1,22 @@
+.DATA
+   COUNT DB 04H
+   VALUE DB 09H, 10H, 05H, 03H
+   RES DB 0
+
+.CODE
+  MAIN PROC
+    MOV AX, DATA
+    MOV DS, AX
+    MOV CL, COUNT
+    DEC CL
+    LEA SI, VALUE
+    MOV AL, [SI]
+   UP:INC SI
+      CMP AL, [SI]
+      JNL NXT
+      MOV AL, [SI]
+   NXT: DEC CL
+        JNZ UP
+        LEA DI, RES
+        MOV [DI], AL
+  END MAIN
